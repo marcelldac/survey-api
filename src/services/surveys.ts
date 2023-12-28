baaf706dc672c7cb10280f5abed9ee3ea043c693
marcelldac <dacmarcell@gmail.com>
2023-12-28 05:22:27 -0300

    refactor: abstraction

--- src/services/surveys.ts
+++ src/services/surveys.ts
@@ -1,12 +1,14 @@
 import { emptyDataError } from "../errors";
 import { Data } from "../utils/types";
 
+type DataIndex = { dataIndex: number };
+
 export class SurveyService {
-  private findDataIndex = (data: Data[], id: string): number => {
+  private findDataIndex = (data: Data[], id: string): DataIndex => {
     const EMPTY_DATA: number = -1;
     const dataIndex = data.findIndex((element) => element.id === id);
     if (dataIndex === EMPTY_DATA) emptyDataError();
-    return dataIndex;
+    return { dataIndex };
   };
 
   public createData = (data: Data[], id: string, title: string): void => {
@@ -18,12 +20,12 @@ export class SurveyService {
   };
 
   public updateData = (data: Data[], id: string, title: string): void => {
-    const dataIndex = this.findDataIndex(data, id);
+    const { dataIndex } = this.findDataIndex(data, id);
     data[dataIndex].title = title;
   };
 
   public removeData = (data: Data[], id: string): void => {
-    const dataIndex = this.findDataIndex(data, id);
+    const { dataIndex } = this.findDataIndex(data, id);
     data.splice(dataIndex, 1);
   };
 }
