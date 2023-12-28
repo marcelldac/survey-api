271489a6ace270094e783f3e42d7768fe4903d51
marcelldac <dacmarcell@gmail.com>
2023-12-28 05:06:08 -0300

    refactor: poo

--- src/controllers/surveys.ts
+++ src/controllers/surveys.ts
@@ -1,60 +1,53 @@
 import { Request, Response } from "express";
 import { Data } from "../utils/types";
 import { randomUUID } from "node:crypto";
-import surveysService from "../services/surveys";
+import { SurveyService } from "../services/surveys";
 
 let data: Data[] = [];
 
-const read = (req: Request, res: Response) => {
-  try {
-    return res.status(200).json(data);
-  } catch (error) {
-    console.error(error);
-    return res.sendStatus(500);
-  }
-};
+export class SurveysController {
+  public read = (req: Request, res: Response) => {
+    try {
+      return res.status(200).json(data);
+    } catch (error) {
+      console.error(error);
+      return res.sendStatus(500);
+    }
+  };
 
-const create = (req: Request, res: Response) => {
-  const id: string = randomUUID();
-  const { title }: Data = req.body;
+  public create = (req: Request, res: Response) => {
+    const id: string = randomUUID();
+    const { title }: Data = req.body;
 
-  try {
-    surveysService.createData(data, id, title);
-    return res.status(201).json(data);
-  } catch (error) {
-    console.error(error);
-    return res.sendStatus(500);
-  }
-};
+    try {
+      new SurveyService().createData(data, id, title);
+      return res.status(201).json(data);
+    } catch (error) {
+      console.error(error);
+      return res.sendStatus(500);
+    }
+  };
 
-const update = (req: Request, res: Response) => {
-  const { id } = req.params;
-  const { title } = req.body;
-  try {
-    surveysService.updateData(data, id, title);
-    return res.status(200).json(data);
-  } catch (error) {
-    console.error(error);
-    return res.sendStatus(500);
-  }
-};
+  public update = (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { title } = req.body;
+    try {
+      new SurveyService().updateData(data, id, title);
+      return res.status(200).json(data);
+    } catch (error) {
+      console.error(error);
+      return res.sendStatus(500);
+    }
+  };
 
-const remove = (req: Request, res: Response) => {
-  const { id } = req.params;
-  try {
-    surveysService.removeData(data, id);
-    return res.status(200).json(data);
-  } catch (error) {
-    console.error(error);
-    return res.sendStatus(500);
-  }
-};
-
-const surveysController = {
-  read,
-  create,
-  update,
-  remove,
-};
-
-export default surveysController;
+  public remove = (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+      new SurveyService().removeData(data, id);
+      return res.status(200).json(data);
+    } catch (error) {
+      console.error(error);
+      return res.sendStatus(500);
+    }
+  };
+}
--- src/services/surveys.ts
+++ src/services/surveys.ts
@@ -3,30 +3,24 @@ import { Data } from "../utils/types";
 
 const EMPTY_DATA: number = -1;
 
-const createData = (data: Data[], id: string, title: string): void => {
-  const payload: Data = {
-    id,
-    title,
+export class SurveyService {
+  public createData = (data: Data[], id: string, title: string): void => {
+    const payload: Data = {
+      id,
+      title,
+    };
+    data.push(payload);
   };
-  data.push(payload);
-};
 
-const updateData = (data: Data[], id: string, title: string): void => {
-  const dataIndex = data.findIndex((element) => element.id === id);
-  if (dataIndex === EMPTY_DATA) emptyDataError();
-  data[dataIndex].title = title;
-};
+  public updateData = (data: Data[], id: string, title: string): void => {
+    const dataIndex = data.findIndex((element) => element.id === id);
+    if (dataIndex === EMPTY_DATA) emptyDataError();
+    data[dataIndex].title = title;
+  };
 
-const removeData = (data: Data[], id: string): void => {
-  const dataIndex = data.findIndex((element) => element.id === id);
-  if (dataIndex === EMPTY_DATA) emptyDataError();
-  data.splice(dataIndex, 1);
-};
-
-const surveysService = {
-  createData,
-  updateData,
-  removeData,
-};
-
-export default surveysService;
+  public removeData = (data: Data[], id: string): void => {
+    const dataIndex = data.findIndex((element) => element.id === id);
+    if (dataIndex === EMPTY_DATA) emptyDataError();
+    data.splice(dataIndex, 1);
+  };
+}
